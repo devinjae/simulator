9b2401c9000188f67ea6709bff3777c04f40b13b
brianadhitya <brianvalentinoadhitya@gmail.com>
2025-10-15 12:39:25 -0700

    Main logic for news

--- backend/app/api/api_v1/endpoints/admin.py
+++ backend/app/api/api_v1/endpoints/admin.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends
-from app.dependencies import get_price_engine, get_news_engine
+from app.dependencies import get_news_engine
 
 router = APIRouter()
 
 @router.post("/news")
-async def create_news(price_engine = Depends(get_price_engine), news_engine = Depends(get_news_engine)):
+async def create_news(news_engine = Depends(get_news_engine)):
+    
     return {"message": "News created successfully"}
--- backend/app/services/news.py
+++ backend/app/services/news.py
@@ -1,16 +1,49 @@
 import time
+import asyncio
 
 class NewsShockSimulator:
     def __init__(self):
-        pass
+        self.news_objects = []
+        self.NEWS_TICK_DELAY = 60
     
     """
     Exponential decay formula
     """
     def calculate(self, news):
+        if not news:
+            return 0 
+        
         now_s = time.time()
         t0_s = news["ts_release_ms"] / 1000
         halflife_s = news["halflife_s"]
         magnitude = news["magnitude"]
         eff = magnitude * 2 ** (-(now_s - t0_s)/halflife_s)
+        
         return eff
+    
+    def get_total_eff(self):
+        total_eff = 0 # 0 is the baseline
+        for news_object in self.news_objects:
+            total_eff += self.calculate(news_object)
+        return total_eff
+
+    def add_news_ad_hoc(self, news_object):
+        self.news_objects.append(news_object)
+    
+    async def add_news_on_tick(self):
+        self.is_running = True
+        while self.is_running:
+            try:
+                news = None # replace with DB query
+                self.add_news_ad_hoc(news)
+                await asyncio.sleep(self.NEWS_TICK_DELAY)
+            except asyncio.CancelledError:
+                self.is_running = False
+                break
+            except Exception as e:
+                # continue
+                await asyncio.sleep(1)
+    
+    
+
+    
