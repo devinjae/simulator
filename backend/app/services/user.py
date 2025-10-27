2f043b579d9be5ea0b1a7c688d6d97536b8fd8af
MatthewAnder <matthewanh@hotmail.com>
2025-10-27 01:42:44 -0700

    add a new variable

--- backend/app/services/user.py
+++ backend/app/services/user.py
@@ -7,15 +7,16 @@ class UserState:
         self.unfulfilled_trades = []
         self.buys = []
         self.realized_pnl = 0
+        self.total_shares = 0
 
     # adds order that is closed and so we call get_avg_price here
     def add_buy(self, ticker, order_price, order_quantity):
-        self.realized_pnl = self.get_avg_price(order_price, order_quantity)
         self.buys.append({
             "ticker": ticker,
             "order_price": order_price,
             "order_quantity": order_quantity
         })
+        self.realized_pnl = self.get_avg_price(order_price, order_quantity)
 
     def add_unfulfilled_trade(self, ticker, order_price, order_quantity):
         self.add_unfulfilled_trade.append({
@@ -25,6 +26,7 @@ class UserState:
         })
 
     def get_avg_price(self, order_price, order_quantity) -> float:
-        current_quantity = sum(qty["order_quantity"] for qty in self.buys)
-        total_quantity = current_quantity + order_quantity
-        return ((self.realized_pnl * current_quantity) + order_price) / total_quantity
+        prev_quantity = self.total_shares
+        self.total_shares += order_quantity
+        avg_price = ((self.realized_pnl * prev_quantity) + order_price) / self.total_shares
+        return avg_price
