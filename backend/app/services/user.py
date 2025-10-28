caee37f6eb451f8d8245758e810d4e9edf5077de
MatthewAnder <matthewanh@hotmail.com>
2025-10-27 17:45:50 -0700

    add comments and new order type

--- backend/app/services/user.py
+++ backend/app/services/user.py
@@ -1,5 +1,15 @@
 
 class UserState:
+    """
+    Example of an order (for reference):
+    {
+        "price": 101.5,
+        "quantity": 5,
+        "ticker": "AAPL",
+        "user_id": "u1",
+        "side": "buy"
+    }
+    """
 
     def __init__(self, user_id):
         self.user_id = user_id
@@ -11,41 +21,35 @@ class UserState:
         self.realized_pnl = 0
 
     # adds order that is closed and so we call get_avg_price here
-    def add_buy(self, ticker, order_price, order_quantity):
-        self.buys.append({
-            "ticker": ticker,
-            "order_price": order_price,
-            "order_quantity": order_quantity
-        })
-        self.realized_pnl = self.get_avg_price(order_price, order_quantity)
-    
+    def add_buy(self, order):
+        self.buys.append(order)
+        self.realized_pnl = self.get_avg_price(order["price"], order["quantity"])
+
     # TODO: implement remove shares FIFO
     def remove_buy(self):
         pass
 
-    def add_unfulfilled_trade(self, ticker, order_price, order_quantity):
-        self.unfulfilled_trades.append({
-            "ticker": ticker,
-            "order_price": order_price,
-            "order_quantity": order_quantity
-        })
+    def add_unfulfilled_trade(self, order):
+        self.unfulfilled_trades.append(order)
 
     def get_avg_price(self, order_price, order_quantity) -> float:
         prev_quantity = self.total_shares
         self.total_shares += order_quantity
-        
+
         # should we check for negatives as well here?
         if self.total_shares == 0:
-            return 0; 
-        
+            return 0
+
         avg_price = (
-            (self.prev_avg_price * prev_quantity) + (order_price * order_quantity)
+            (self.prev_avg_price * prev_quantity) +
+            (order_price * order_quantity)
         ) / self.total_shares
-        
+
         self.prev_avg_price = avg_price
-        
+
         return avg_price
-    
+
     # then this would be a separate thing right?
+    # TODO: implement pnl calculation
     def calculate_realized_pnl(self):
         self.realized_pnl = 0
