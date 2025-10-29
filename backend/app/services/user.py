d78c55f41eb9a8cf261039dae2e77d61638405f9
MatthewAnder <matthewanh@hotmail.com>
2025-10-28 17:12:32 -0700

    add portfolio and sell function

--- backend/app/services/user.py
+++ backend/app/services/user.py
@@ -1,42 +1,67 @@
 
 class UserState:
-    """
-    Example of an order (for reference):
+    """ Example of a portfolio
+    "ticker": [(qty, price), (qty, price)]
+
     {
-        "price": 101.5,
-        "quantity": 5,
-        "ticker": "AAPL",
-        "user_id": "u1",
-        "side": "buy"
+        "AAPL": [(1, 120), (2, 125)],
+        "MSFT": [(3, 310)]
     }
     """
 
     def __init__(self, user_id):
         self.user_id = user_id
         self.cash = 0
-        self.unfulfilled_trades = []
-        self.buys = []
+
+        self.portfolio = [] # contains user's portfolio
+        self.unfulfilled_trades = [] # orders that have not been fulfilled or position is not closed
+        self.fulfilled_trades = []  # orders that have been fulfilled or position is closed
+
         self.total_shares = 0
         self.prev_avg_price = 0
-        self.realized_pnl = 0
+        self.total_realized_pnl = 0
 
-    # adds order that is closed and so we call get_avg_price here
-    def add_buy(self, order):
-        self.buys.append(order)
-        self.realized_pnl = self.get_avg_price(order["price"], order["quantity"])
-
-    # TODO: implement remove shares FIFO
-    def remove_buy(self):
-        pass
-
+    # adds order that has not been closed
     def add_unfulfilled_trade(self, order):
         self.unfulfilled_trades.append(order)
+
+    # adds order that has been closed and so we call get_avg_price here
+    def add_fulfilled_trades(self, order):
+        self.fulfilled_trades.append(order)
+        if order["side"] == "buy":
+            self.portfolio[order["ticker"]].append((order["quantity"], order["price"]))
+        elif order["side"] == "sell":
+            self.sell_shares(order["ticker"], order["quantity"], order["price"])
+
+    # user sell shares and would remove the shares based on FIFO
+    def sell_shares(self, ticker, sell_qty, sell_price):
+        idx = 0
+
+        remaining = sell_qty
+        lots = self.portfolio[ticker]
+
+        if not lots:
+            raise ValueError(f"Ticker {ticker} is not found!")
+
+        for buy_qty, buy_price in lots:
+            if (remaining >= buy_qty):
+                lots.pop(0)
+                remaining -= buy_qty
+                idx += 1
+            else:
+                lots[idx] = (buy_qty - remaining, buy_price)
+                break
+
+        if remaining > 0:
+            raise ValueError(
+                f"Not enough shares to sell: tried {sell_qty}, only sold {sell_qty - remaining}")
+
+        # TODO calculate realized pnl here
 
     def get_avg_price(self, order_price, order_quantity) -> float:
         prev_quantity = self.total_shares
         self.total_shares += order_quantity
 
-        # should we check for negatives as well here?
         if self.total_shares == 0:
             return 0
 
@@ -49,7 +74,6 @@ class UserState:
 
         return avg_price
 
-    # then this would be a separate thing right?
-    # TODO: implement pnl calculation
+    # TODO implement pnl calculation
     def calculate_realized_pnl(self):
-        self.realized_pnl = 0
+        pass
